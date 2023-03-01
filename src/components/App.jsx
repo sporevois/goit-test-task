855391274f1d2efeec5f72bd32456df47cbb943a
Stanislav Sporevoi <stanislav.sporevoi@gmail.com>
2023-03-02 01:19:00 +0200

    background finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styles from '../components/App.module.css';
+import { ReactComponent as Logo } from './Logo.svg';
 
 export const App = () => {
   const [followersCount, setFollowersCount] = useState(100500);
@@ -25,17 +26,22 @@ export const App = () => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.card}>
-        <p className={styles.text}>777 TWEETS</p>
-        <p className={styles.text}>{formatCount(followersCount)} FOLLOWERS</p>
-        <button
-          className={styles.btn}
-          style={isFollowing ? { backgroundColor: '#5CD3A8' } : {}}
-          type="button"
-          onClick={toggleFollowingStatus}
-        >
-          {followingStatus}
-        </button>
+      <div className={styles.background}>
+        <div className={styles.card}>
+          <div className={styles.logo}>
+            <Logo />
+          </div>
+          <p className={styles.text}>777 TWEETS</p>
+          <p className={styles.text}>{formatCount(followersCount)} FOLLOWERS</p>
+          <button
+            className={styles.btn}
+            style={isFollowing ? { backgroundColor: '#5CD3A8' } : {}}
+            type="button"
+            onClick={toggleFollowingStatus}
+          >
+            {followingStatus}
+          </button>
+        </div>
       </div>
     </div>
   );
