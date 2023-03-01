319302ac9065606637f1b1cfd8e9b7cda787aac8
Stanislav Sporevoi <stanislav.sporevoi@gmail.com>
2023-03-01 22:41:45 +0200

    finish logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
+import React, { useState } from 'react';
+import styles from '../components/App.module.css';
+
 export const App = () => {
+  const [followersCount, setFollowersCount] = useState(100500);
+  const [followingStatus, setFollowingStatus] = useState('FOLLOW');
+  const [isFollowing, setIsFollow] = useState(false);
+
+  const toggleFollowingStatus = () => {
+    if (isFollowing === false) {
+      setIsFollow(true);
+      setFollowingStatus('FOLLOWING');
+      setFollowersCount(prev => prev + 1);
+    } else if (isFollowing === true) {
+      setIsFollow(false);
+      setFollowingStatus('FOLLOW');
+      setFollowersCount(prev => prev - 1);
+    }
+  };
+
+  const formatCount = number => {
+    const numberFormat = new Intl.NumberFormat('en-US');
+    return numberFormat.format(number);
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <p className={styles.text}>777 TWEETS</p>
+        <p className={styles.text}>{formatCount(followersCount)} FOLLOWERS</p>
+        <button
+          className={styles.btn}
+          style={isFollowing ? { backgroundColor: '#5CD3A8' } : {}}
+          type="button"
+          onClick={toggleFollowingStatus}
+        >
+          {followingStatus}
+        </button>
+      </div>
     </div>
   );
 };
